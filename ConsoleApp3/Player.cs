b8e05cf020fa0fce771b6d4dcb9193143a7aa03c
Kripchek <133848904+Kripchek@users.noreply.github.com>
2023-06-05 12:22:10 +0300

    111

---
+++ ConsoleApp3/Player.cs
@@ -0,0 +1,76 @@
+﻿namespace ConsoleApp3;
+
+public class Player
+{
+    private string _name;
+    private double _health;
+    private int _damage;
+    private int _defense;
+    private int _mana;
+    private double _experience;
+    private int _lvl;
+    private int _gold;
+    public int _idcharcter;
+    static public int _iddcharcter = 1;
+
+    public Player(string name, double health, int damage, int defense, int mana,int lvl, double experience, int gold)
+    {
+        _name = name;
+        _health = health;
+        _damage = damage;
+        _defense = defense;
+        _mana = mana;
+        _lvl = lvl;
+        _gold = gold;
+        _idcharcter = _iddcharcter++;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public int Damage
+    {
+        get => _damage;
+        set => _damage = value;
+    }
+
+    public int Defense
+    {
+        get => _defense;
+        set => _defense = value;
+    }
+
+    public int Mana
+    {
+        get => _mana;
+        set => _mana = value;
+    }
+
+    public double Experience
+    {
+        get => _experience;
+        set => _experience = value;
+    }
+
+    public double Health
+    {
+        get => _health;
+        set => _health = value;
+    }
+
+    public int Lvl
+    {
+        get => _lvl;
+        set => _lvl = value;
+    }
+
+    public int Gold
+    {
+        get => _gold;
+        set => _gold = value;
+    }
+ 
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace ConsoleApp3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Inventory> inventory = new List<Inventory>()
+            {
+                new("chto-to1", 7),
+                new("chto-to2", 5),
+                new("chto-to3", 9),
+                new("chto-to4", 1),
+                new("chto-to5", 1),
+                new("chto-to6", 1),
+                new("chto-to7", 2),
+                new("chto-to8", 1),
+            };
+           
+            Battle battle = new Battle();
+            battle.Battlechek();
+
+            void Test1()
+            {
+                Console.WriteLine("список ваших предметов");
+                foreach (var invent in inventory)
+                {
+                    Console.WriteLine($"{invent._id}, {invent.Name}, {invent.Count}");
+                }
+            }
+
+            void TestAddItem(string item, int amount)
+            {
+                Console.WriteLine($"вам выпал {item} в количестве {amount}. вы хотите его добавить в свой инвентарь? (Y)es/(N)o");
+                string choiseadditem = Console.ReadLine();
+                if ( choiseadditem == "Y" || choiseadditem == "y"|| choiseadditem == "У" || choiseadditem == "у") 
+                {
+                    if (inventory.Count < inventory.Capacity)
+                    {
+                        inventory.Add(new(item, amount));
+                    }
+                    else
+                    {
+                        Console.WriteLine("у вас нет места в инвентаре");
+                    }
+                }
+                else if (choiseadditem == "N" || choiseadditem == "n") 
+                {
+                    Console.WriteLine("Правильно, нахер этот предмет нужен :3");
+                }
+            }
+
+            void Test2()
+            {
+                Console.WriteLine("напишите номер предмета(id) который вы хотите выкинуть:");
+                int x = int.Parse(Console.ReadLine());
+                foreach (var invent in inventory)
+                {
+                    if (invent._id == x)
+                    {
+                        Console.WriteLine("вы хотите выкинуть все предметы(dropall) или только один(drop)");
+                        string choisedrop = Console.ReadLine();
+                        if (choisedrop == "dropall")
+                        {
+                            inventory.Remove(invent);
+                            break;
+                        }
+                        else if (choisedrop == "drop")
+                        {
+                                invent.Count -= 1;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
